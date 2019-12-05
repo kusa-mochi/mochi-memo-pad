54c6b9436c2eae9fce583a7d8eafea332204599f
kusa-mochi <whoatemyapplepie@gmail.com>
2019-12-05 22:40:03 +0900

    メモのタイトル表示領域を追加。

--- src/src/components/Header.vue
+++ src/src/components/Header.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="header">
-      <button class="menu-button">開く</button>
-      <button class="menu-button">保存</button>
+    <button class="menu-button">開く</button>
+    <button class="menu-button">保存</button>
+    <div class="memo-title">
+      <span class="memo-title__no-saved">*</span><span class="memo-title__main" title="レポートのタイトルカレーはうまい寿司もうまいラーメンもうまいあれもうまいこれもうまい">レポートのタイトルカレーはうまい寿司もうまいラーメンもうまいあれもうまいこれもうまい</span>
+    </div>
   </div>
 </template>
 
@@ -18,23 +21,57 @@ export default {
 
 <style lang="scss" scoped>
 $headerBackgroundColor: #0e521d;
+$menuItemHeight: 48px;
 #header {
   width: 100%;
-  height: 48px;
+  min-height: $menuItemHeight;
   background-color: $headerBackgroundColor;
   position: fixed;
 
-  .menu-button {
-      width: 100px;
-      height: 100%;
-      background-color: transparent;
-      color: white;
-      border: none;
-      outline: none;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+  align-content: flex-start;
 
-      &:hover {
-          background-color: #127a29;
-      }
+  .menu-button {
+    width: 100px;
+    height: $menuItemHeight;
+    background-color: transparent;
+    color: white;
+    border: none;
+    outline: none;
+
+    &:hover {
+      background-color: #127a29;
+    }
+  }
+
+  .memo-title {
+    margin: auto 8px;
+    max-width: 500px;
+    height: $menuItemHeight;
+    background-color: transparent;
+    color: white;
+    border: none;
+    outline: none;
+
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: center;
+    align-items: center;
+
+    &__no-saved {
+      font-size: 20px;
+    }
+
+    &__main {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
   }
 }
 </style>
