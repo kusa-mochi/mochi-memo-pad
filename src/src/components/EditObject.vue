a99ef76c6efc92264bdb0305799ed5688283976e
kusa-mochi <whoatemyapplepie@gmail.com>
2019-12-07 22:21:54 +0900

    add tool icon area to edit items. change name "edit item" to "edit object".

---
+++ src/src/components/EditObject.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="edit-object">
+    <div class="edit-object__body">abcdefghijklmnop</div>
+    <div class="edit-object__tool-icons">menu icons are here.</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EditObject",
+  data() {
+    return {
+      sampleProp: 123
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+$tool_icon_height: 32px;
+
+.edit-object {
+  position: relative;
+  width: 100%;
+  height: 100px;
+
+  .edit-object__body {
+  }
+
+  .edit-object__tool-icons {
+    position: absolute;
+    top: -$tool_icon_height;
+    left: 0;
+    height: $tool_icon_height;
+    background-color: #169632;
+    color: white;
+    opacity: 0;
+  }
+
+  &:hover {
+    box-shadow: 0px 0px 0px 1px rgba(0, 0, 0, 0.4);
+
+    .edit-object__tool-icons {
+      opacity: 1;
+    }
+  }
+}
+</style>
--- src/src/components/EditorMain.vue
+++ src/src/components/EditorMain.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="editor-main">
-      <edit-item v-for="item in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]"></edit-item>
+      <edit-object v-for="item in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]"></edit-object>
   </div>
 </template>
 
 <script>
-import EditItem from "./EditItem.vue";
+import EditObject from "./EditObject.vue";
 
 export default {
   name: "EditorMain",
   components: {
-    EditItem
+    EditObject
   },
   data() {
     return {
