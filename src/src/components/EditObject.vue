b2a93f45d50caead27981840fa7194144e3ef41d
kusa-mochi <whoatemyapplepie@gmail.com>
2019-12-08 16:38:27 +0900

    make plus icon and buttons on edit objects.

--- src/src/components/EditObject.vue
+++ src/src/components/EditObject.vue
@@ -2,23 +2,34 @@
   <div class="edit-object">
     <div class="edit-object__body">abcdefghijklmnop</div>
     <div class="edit-object__tool-icons">menu icons are here.</div>
+    <div class="edit-object__add-prev-button">
+      <plus-icon></plus-icon>
+    </div>
+    <div class="edit-object__add-next-button">
+      <plus-icon></plus-icon>
+    </div>
   </div>
 </template>
 
 <script>
+import PlusIcon from "./icons/PlusIcon.vue";
+
 export default {
   name: "EditObject",
   data() {
     return {
       sampleProp: 123
     };
+  },
+  components: {
+    PlusIcon
   }
 };
 </script>
 
 <style lang="scss" scoped>
-$tool_icon_height: 32px;
 @import "../assets/colors";
+$tool_icon_height: 24px;
 
 .edit-object {
   position: relative;
@@ -38,10 +49,30 @@ $tool_icon_height: 32px;
     opacity: 0;
   }
 
+  .edit-object__add-prev-button,
+  .edit-object__add-next-button {
+    position: absolute;
+    width: 24px;
+    height: 24px;
+    opacity: 0;
+  }
+
+  .edit-object__add-prev-button {
+    top: -12px;
+    left: calc(50% - 12px);
+  }
+
+  .edit-object__add-next-button {
+    top: calc(100% - 12px);
+    left: calc(50% - 12px);
+  }
+
   &:hover {
     box-shadow: 0px 0px 0px 1px rgba(0, 0, 0, 0.4);
 
-    .edit-object__tool-icons {
+    .edit-object__tool-icons,
+    .edit-object__add-prev-button,
+    .edit-object__add-next-button {
       opacity: 1;
     }
   }
