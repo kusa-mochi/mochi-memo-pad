d55541109317fd9485f6ac86a6c6a8e325e93fd9
fujimaki <fujimaki@shimadzu.co.jp>
2019-12-10 10:35:44 +0900

    remove unused code.

--- src/src/components/EditorComponents/ParagraphEditor.vue
+++ src/src/components/EditorComponents/ParagraphEditor.vue
@@ -20,15 +20,6 @@ export default {
   text-align: left;
   outline: none;
 
-  &::before {
-    position: absolute;
-    content: "";
-    top: -8px;
-    right: -8px;
-    bottom: -8px;
-    left: -8px;
-  }
-
   &:focus {
     box-shadow: 0px 0px 0px 1px rgba(0, 0, 0, 0.8);
   }
