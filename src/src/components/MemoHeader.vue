d40a809033cd97ce121107c7c51989950c6cb4a4
fujimaki <fujimaki@shimadzu.co.jp>
2019-12-12 10:51:03 +0900

    update header layout.

--- src/src/components/MemoHeader.vue
+++ src/src/components/MemoHeader.vue
@@ -1,10 +1,21 @@
 <template>
   <div id="memo-header">
-    <button class="menu-button">Open</button>
-    <button class="menu-button">Save</button>
-    <button class="menu-button">Export</button>
-    <div class="memo-title">
-      <span class="memo-title__no-saved">*</span><span class="memo-title__main" title="Title is here. Title is here. Title is here. Title is here. Title is here. Title is here.">Title is here. Title is here. Title is here. Title is here. Title is here. Title is here.</span>
+    <div class="memo-header__left">
+      <button class="menu-button">Open</button>
+      <button class="menu-button">Save</button>
+      <button class="menu-button">Export</button>
+      <div class="memo-title">
+        <span class="memo-title__no-saved">*</span>
+        <span
+          class="memo-title__main"
+          title="Title is here. Title is here. Title is here. Title is here. Title is here. Title is here."
+        >Title is here. Title is here. Title is here. Title is here. Title is here. Title is here.</span>
+      </div>
+    </div>
+    <div class="memo-header__right">
+      <button class="minimize-button">_</button>
+      <button class="maximize-button">□</button>
+      <button class="close-button">x</button>
     </div>
   </div>
 </template>
@@ -23,7 +34,21 @@ export default {
 <style lang="scss" scoped>
 @import "../assets/colors";
 
-$menuItemHeight: 48px;
+$menuItemHeight: 40px;
+
+@mixin header_button() {
+  height: $menuItemHeight;
+  padding: 0 16px;
+  background-color: transparent;
+  color: white;
+  font-size: 14px;
+  border: none;
+  outline: none;
+
+  &:hover {
+    background-color: #127a29;
+  }
+}
 
 #memo-header {
   width: 100%;
@@ -33,48 +58,67 @@ $menuItemHeight: 48px;
 
   display: flex;
   flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: flex-start;
+  flex-wrap: wrap-reverse;
+  justify-content: space-between;
   align-items: flex-start;
-  align-content: flex-start;
+  align-content: flex-end;
 
-  .menu-button {
-    height: $menuItemHeight;
-    padding: 0 16px;
-    background-color: transparent;
-    color: white;
-    font-size: 14px;
-    border: none;
-    outline: none;
+  -webkit-app-region: drag;
 
-    &:hover {
-      background-color: #127a29;
-    }
+  * {
+    -webkit-app-region: no-drag;
   }
 
-  .memo-title {
-    margin: auto 8px;
-    max-width: 500px;
-    height: $menuItemHeight;
-    background-color: transparent;
-    color: white;
-    border: none;
-    outline: none;
-
+  .memo-header__left {
     display: flex;
     flex-direction: row;
     flex-wrap: nowrap;
-    justify-content: center;
-    align-items: center;
+    justify-content: flex-start;
+    align-items: flex-start;
 
-    &__no-saved {
-      font-size: 20px;
+    .menu-button {
+      @include header_button();
     }
 
-    &__main {
-      overflow: hidden;
-      text-overflow: ellipsis;
-      white-space: nowrap;
+    .memo-title {
+      margin: auto 8px;
+      max-width: 500px;
+      height: $menuItemHeight;
+      background-color: transparent;
+      color: white;
+      border: none;
+      outline: none;
+
+      display: flex;
+      flex-direction: row;
+      flex-wrap: nowrap;
+      justify-content: center;
+      align-items: center;
+
+      &__no-saved {
+        font-size: 20px;
+      }
+
+      &__main {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+    }
+  }
+
+  .memo-header__right {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: flex-end;
+    align-items: flex-start;
+
+    .minimize-button,
+    .maximize-button,
+    .close-button {
+      @include header_button();
+      width: $menuItemHeight;
     }
   }
 }
