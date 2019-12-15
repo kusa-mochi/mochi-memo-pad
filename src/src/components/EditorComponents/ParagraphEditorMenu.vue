3c4dd728eadb1b65e509d7748b62b9a609e0040c
kusa-mochi <whoatemyapplepie@gmail.com>
2019-12-15 20:38:15 +0900

    make icon structure base.

--- src/src/components/EditorComponents/ParagraphEditorMenu.vue
+++ src/src/components/EditorComponents/ParagraphEditorMenu.vue
@@ -1,21 +1,51 @@
 <template>
-  <div class="paragraph-editor-menu">menu icons are here.</div>
+  <div class="paragraph-editor-menu">
+    <button>
+      <icon icon-name="three-dots" size="24"></icon>
+    </button>
+    <button>
+      <icon icon-name="three-dots" size="24"></icon>
+    </button>
+    <button>
+      <icon icon-name="three-dots" size="24"></icon>
+    </button>
+    <button>
+      <icon icon-name="three-dots" size="24"></icon>
+    </button>
+    <button>
+      <icon icon-name="three-dots" size="24"></icon>
+    </button>
+  </div>
 </template>
 
 <script>
+import Icon from "../Icon.vue";
 export default {
   name: "ParagraphEditorMenu",
-  data() {
-    return {
-      sampleProp: 123
-    };
+  components: {
+    Icon
   }
 };
 </script>
 
 <style lang="scss" scoped>
-@import "../../assets/colors";
+@import "../../assets/assets";
 .paragraph-editor-menu {
-    background-color: $theme_color;
+  height: $tool_icon_height;
+  border: 1px $theme_color solid;
+
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+  align-content: flex-start;
+
+  button {
+    height: 100%;
+    padding: 3px;
+    background-color: white;
+    border: none;
+  }
 }
 </style>
