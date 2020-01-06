47ff8ba28f8440f6238aca933238a654342368bd
fujimaki <fujimaki@shimadzu.co.jp>
2020-01-06 10:31:25 +0900

    make a component template for images based on paragraph editor component.

---
+++ src/src/components/EditorComponents/ImageEditor.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="image-editor"
+    role="textbox"
+    contenteditable="true"
+  >{{this.$store.state.editingData[this.id].data}}</div>
+</template>
+
+<script>
+export default {
+  name: "ImageEditor",
+  props: {
+    id: {
+      type: Number,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.image-editor {
+  padding: 8px 16px;
+  font-size: 14px;
+  text-align: left;
+  outline: none;
+
+  &:focus {
+    box-shadow: 0px 0px 0px 1px rgba(0, 0, 0, 0.8);
+  }
+}
+</style>
---
+++ src/src/components/EditorComponents/ImageEditorMenu.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="image-editor-menu">
+    <button>
+      <icon icon-name="three-dots" size="24"></icon>
+    </button>
+    <button>
+      <icon icon-name="three-dots" size="24"></icon>
+    </button>
+    <button>
+      <icon icon-name="three-dots" size="24"></icon>
+    </button>
+  </div>
+</template>
+
+<script>
+import Icon from "../Icon.vue";
+export default {
+  name: "ImageEditorMenu",
+  components: {
+    Icon
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/assets";
+.image-editor-menu {
+  height: $tool_icon_height;
+  border: 1px $theme_color solid;
+
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+  align-content: flex-start;
+
+  button {
+    height: 100%;
+    padding: 3px;
+    background-color: white;
+    border: none;
+  }
+}
+</style>
