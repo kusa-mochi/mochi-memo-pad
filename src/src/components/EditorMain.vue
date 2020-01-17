5a2e203ba14ace5ebc9e22129f16b907c5d9a316
fujimaki <fujimaki@shimadzu.co.jp>
2020-01-17 10:38:24 +0900

    add comment.

--- src/src/components/EditorMain.vue
+++ src/src/components/EditorMain.vue
@@ -29,6 +29,7 @@ export default {
         return this.$store.state.editingData;
       },
       set(val) {
+        // do not use "dispatch", otherwise data is not synced to UI.
         this.$store.commit("updateEditingDataMutation", val);
       }
     }
