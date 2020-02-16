087bbc8289256c09b3887263de7014a8729fe558
kusa-mochi <whoatemyapplepie@gmail.com>
2020-02-16 20:10:30 +0900

    update initial data.

--- src/src/store/index.js
+++ src/src/store/index.js
@@ -6,7 +6,12 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     title: "",
-    editingData: [],
+    editingData: [
+      {
+        editorType: "paragraph",
+        data: ""
+      }
+    ],
     isDataSaved: true
   },
   mutations: {
