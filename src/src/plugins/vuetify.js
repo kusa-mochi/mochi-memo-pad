a287dcc14ee371cf411487403d5fff295df015cf
kusa-mochi <whoatemyapplepie@gmail.com>
2020-03-21 19:02:00 +0900

    make light color theme.

--- src/src/plugins/vuetify.js
+++ src/src/plugins/vuetify.js
@@ -4,4 +4,17 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: "#419C55",
+                secondary: "#60B373",
+                error: "#e91e63",
+                warning: "#ff9800",
+                accent: "#3f51b5",
+                info: "#607d8b",
+                success: "#009688"
+            }
+        }
+    }
 });
