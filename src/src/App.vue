6429c2baededb6567c19a8d3adf25ff969af8175
kusa-mochi <whoatemyapplepie@gmail.com>
2020-03-21 18:41:52 +0900

    install vuetify.

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,62 +1,60 @@
 <template>
-  <div id="app">
-    <memo-header></memo-header>
-    <div class="editor-container">
-      <memo-editor></memo-editor>
-    </div>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import MemoHeader from "./components/MemoHeader.vue";
-import MemoEditor from "./components/MemoEditor.vue";
+import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: "app",
+  name: 'App',
+
   components: {
-    MemoHeader,
-    MemoEditor
-  }
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
 };
 </script>
-
-<style lang="scss">
-@import "assets/assets";
-
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-html,
-body {
-  width: 100%;
-  height: 100%;
-  background-color: rgba($theme_color, 0.1);
-}
-.mochi-icon {
-  fill: $theme_color;
-}
-#app {
-  display: flex;
-  flex-direction: column;
-  flex-wrap: nowrap;
-  justify-content: flex-start;
-  align-items: center;
-
-  width: 100%;
-  height: 100%;
-
-  .editor-container {
-    position: relative;
-    height: calc(100% - #{$header_height});
-    background-color: white;
-  }
-}
-
-@media print {
-  html, body {
-    background-color: transparent;
-  }
-}
-</style>
